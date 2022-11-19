7056de34056f89c9d208631c61d61125cad61271
evgenii <evgenii.lyabov@mail.ru>
2022-11-20 00:20:56 +0300

    lesson1

---
+++ lesson1/hello.py
@@ -0,0 +1,144 @@
+#print('hello')
+#value=None
+##b=1.23
+#print(type(a))
+#print(type(b))
+#value=2566
+#print(type(value))
+#s='hello world'
+#print(a,'-', b,'-',s)
+#print('{2} - {1} - {0}'.format(a,b,s)) # можно менять местами
+#print(f'{a} - {b} - {s}')
+#f=True
+#print(f)
+
+#list=['ddf', 'fef', 446, '+', 'dd', 1.555, True]
+#print(list)
+
+#print('Введите а');
+#a=float(input())
+#print('Введите b');
+#b=float(input())
+#print(a,'+', b,'=', a+b)
+#print('{1} - {0}'.format(a,b)) # можно менять местами
+#print(f'{a} - {b}')
+
+# Арифметические операции
+
+#a=3
+#a+=5
+#print(a)
+
+# Логические операции
+
+# f= [1,2,3,4]
+# # print(f)
+# # print(not 5 in f)
+# is_odd=not f[0]%2
+# print(is_odd)
+
+# Управляющие конструкции
+# if, if-else
+
+# a=int(input('введите число а'))     # поиск максимального числа
+# b=int(input('введите число b'))
+# if a>b:
+#     print(a)
+# else:
+#     print(b)
+
+# while
+
+# original =23            #  перевернуть число
+# inverted = 0
+# while original != 0:
+#     inverted = inverted *10+(original%10)
+#     original//=10
+#     print(original)
+# else:
+#     print('пожалуй')
+#     print('хватит )')
+# print(inverted)
+
+# for
+
+# for i in 1,2,3,4,5,6:       # вщзведение чисел в степень
+#     print(i**3)
+
+# list=[1,2,3,10,4,5]
+# for i in list:       # вщзведение чисел в степень
+#     # print(i)
+
+# for i in range(2, 50, 6):       # выводит числа от 2 до 49 c прирощением 6
+#     print(i)
+
+# for i in 'gji kjio':       # выводит все буквы с пробелом и знаками
+#     print(i)
+# help(text.isalpha)              # встроенная функция помощи
+# text = 'съешь ещё этих мягких французских булок'
+# print(len(text))                        # 39
+# print('ещё'in text)                     # True
+# print(text.isdigit())                   # False
+# print(text.islower())                   # True
+# print(text.replace('ещё', 'ЕЩЁ'))       # меняет прописные на заглавные
+
+# for c in text:
+#     print(c)                            # выводит текст побуквенно
+
+# text = 'съешь ещё этих мягких французских булок' # с конца массива элементы считаются с -1
+# print(text[0])                     # c
+# print(text[1])                     # ъ
+# #print(text[len(text)])             # IndexError
+# print(text[len(text)-1])           # к
+# print(text[-5])                    # б
+# print(text[:])                     # print(text)
+# print(text[:2])                     # съ
+# print(text[len(text)-2:])          # ок
+# print(text[2:9])                   # ешь ещё
+# print(text[6:-18])                 # ещё этих мягких
+# print(text[0:len(text):6])         # сеикакл
+# print(text[::6])                   # сеикакл
+# text=text[2:9]+text[-5]+text[:2]   # ...
+
+# Списки   (базовые АПИ работы со списками)
+
+# numbers=[1,2,3,4,5]
+# print(numbers)                  # [1,2,3,4,5]
+# ran=range(1,6)
+# print(type(ran))
+# numbers=list(ran)
+# print(type(numbers))
+# print(numbers)                  # [1,2,3,4,5]
+# numbers[0]=10
+# print(f'{len(numbers)} len')        # интерполяция
+# print(numbers)                  # [10,2,3,4,5]
+# for i in numbers:
+#     i*=2
+#     print(i)                    # [20,4,6,8,10]
+# print(numbers)                  # [10,2,3,4,5]
+
+# colors=['red','green','blue']
+# for e in colors:
+#     print(e)                        # red green blue
+# for e in colors:
+#     print(e*2)                      # redred greengreen blueblue
+# colors.append('gray')               # добавить в конкец
+# print(colors==['red','green','blue','gray'])        # True
+# colors.remove('red')               # del colors[0]  # удалить элемент
+
+# Функции
+
+def f(x):
+    if x ==1:
+        return 'целое'
+    elif x ==2.3:
+        return 23
+    else:
+        return
+
+arg =8
+print(f(arg))
+print(type(f(arg)))
+
+
+

920dddc9bfca6dc21bbc5562abbe9f9845a9f780
evgenii <evgenii.lyabov@mail.ru>
2022-11-29 10:07:17 +0300

    hwdz2

--- seminar2/homeworkdz2.py
+++ seminar2/homeworkdz2.py
@@ -4,3 +4,16 @@
 # ⋀ - "И"
 # ¬ - "Не"
 
+xpred = [True, False]
+ypred = [True, False]
+zpred = [True, False]
+for i in range(2):
+    xpred[i]
+for x in xpred:
+    for y in ypred:
+        for z in zpred:
+            print(x, y, z)
+            res1 = not (x or y or z)
+            res2 = (not x) and (not y) and (not z)
+            print(res1 == res2)
+

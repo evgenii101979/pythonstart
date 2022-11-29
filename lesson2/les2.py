6f41342980aa99e1d2145bbd531514592384390f
evgenii <evgenii.lyabov@mail.ru>
2022-11-29 18:01:39 +0300

    les2sev2

--- lesson2/les2.py
+++ lesson2/les2.py
@@ -74,16 +74,105 @@
 # t=tuple(colors)
 # print(t)                        # ('red','green','blue')
 
-a = (3, 4)
-print(a)
-print(a[0])
+# a = (3, 4, 5)
+# # print(a)
+# # print(a[0])
+# for item in a:
+#     print(item)             # выводит все числа 3 4 5
 
-# #  9       СЛОВАРИ(НЕУПОРЯДОЧЕННЫЕ КОЛЛЕКЦИИ ПРОИЗВОЛЬНЫХ
+# t = tuple(['red', 'green', 'blue'])
+# red, green, blue = t
+# print('r: {} g: {} b: {}'.format(red, green, blue)) # r: red g: green b: blue
+
+#  9       СЛОВАРИ(НЕУПОРЯДОЧЕННЫЕ КОЛЛЕКЦИИ ПРОИЗВОЛЬНЫХ
 #            ОБЪЕКТОВ С ДОСТУПОМ ПО КЛЮЧУ)
 
-# dictionary={}
-# dictionary=\
+# dictionary = {}
+# dictionary = \
 #     {
-#         'up': ''
+#         'up': '↑',
+#         'left': '←',
+#         'down': '↓',
+#         'right': '→'
 #     }
-#
+# # print(dictionary)     # {'up': '↑', 'left': '←', 'down': '↓', 'right': '→'}
+# # print(dictionary['left'])   # ←
+# # for k in dictionary.keys():
+# #     print(k)                    # up left down right ключи выдает
+# # for k in dictionary.values():
+# #     print(k)                    # ↑ ← ↓ → выдает значения
+# for v in dictionary:
+#     print(v)                  # up left down right проходит по всему словарю
+# for v in dictionary:
+#     print(dictionary[v])        # ↑ ← ↓ → только значения
+
+# 10     МНОЖЕСТВА
+
+# ИЗМЕНЯЕМЫЕ
+
+# colors = {'red', 'green', 'blue'}
+# print(colors)           # {'blue', 'red', 'green'}
+# colors.add('gray')
+# print(colors)           # {'blue', 'red', 'green', 'gray'}
+# colors.remove('red')
+# print(colors)           # {'gray', 'green', 'blue'}
+# colors.discard('red')
+# print(colors)           # {'gray', 'blue', 'green'}
+# colors.clear()
+# print(colors)           # set()
+
+# a = {1,2,3,5,8}
+# b = {2,6,7,4,21}
+# # c = a.copy()              # {1, 2, 3, 5, 8}
+# # u = a.union(b)            # {1, 2, 3, 4, 5, 6, 7, 8, 21}
+# # i = a.intersection(b)     # {2}
+# # dl = a.difference(b)      # {8, 1, 3, 5}
+# # dr = b.difference(a)      # {4, 21, 6, 7}
+# q = a \
+#     .union(b) \
+#     .difference(a.intersection(b))  # {1, 3, 4, 5, 6, 7, 8, 21}
+# print(q)
+
+# НЕИЗМЕНЯЕМЫЕ
+
+# s = frozenset(a)      # добавить и удалить ничего нельзя
+
+# 11       СПИСКИ
+
+# list1 = [1,2,3,4,5]
+# list2 = list1
+# list1[0] = 123          # меняя данные в списке 1 меняются и в списке 2
+# list2[1] = 345          # меняя данные в списке 2 меняются и в списке 1
+# print()
+# for e in list1:
+#     print(e)            # 1 2 3 4 5
+# print()
+# for e in list2:
+#     print(e)            # 1 2 3 4 5
+
+# list1 = [1,2,3,4,5]
+
+# print(list1.append(11))      # добавляет цифру 11 последним элементом
+# print(list1)
+
+# print(list1.insert(2, 11))      # добавляет цифру 11 2-ым элементом
+# print(list1)
+
+# print(list1.pop(2))         # 3       удаляет 2 элемент 
+# print(list1)                # [1, 2, 4, 5]
+
+# print(list1)                # [1, 2, 3, 4, 5]
+# print(list1.pop())          # 5
+# print(list1)                # [1, 2, 3, 4]
+# print(list1.pop())          # 4
+# print(list1)                # [1, 2, 3]
+# print(list1.pop())          # 3
+# print(list1)                # [1, 2]
+# print(list1.pop())          # 2
+# print(list1)                # [1]
+# print(list1.pop())          # 1
+
+
+
+
+

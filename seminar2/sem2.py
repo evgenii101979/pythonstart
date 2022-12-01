5f752de19e48b047af110b7e5096ba401df254c5
evgenii <evgenii.lyabov@mail.ru>
2022-12-01 16:47:23 +0300

    sevdz1

--- seminar2/sem2.py
+++ seminar2/sem2.py
@@ -4,9 +4,9 @@
 # *Пример:*
 # - Для N = 5: 1, -3, 9, -27, 81
 
-# n = 5
-# for i in range(n):
-#     print(f' {(-3)**i} ', end='')
+n = 6
+for i in range(n):
+    print(f' {(-3)**i} ', end='')
 
 # '2. Для натурального n создать словарь индекс-значение, 
 # состоящий из элементов последовательности 3n + 1.
@@ -23,14 +23,14 @@
 # '3. Напишите программу, в которой пользователь будет задавать две строки, 
 # а программа - определять количество вхождений одной строки в другой.
 
-a = input()
-b = input()
-count = 0
-for i in range(len(a)):
-    if a[i:i+len(b)] == b:
-        count += 1
-print(count)
-print(a.count(b))
+# a = input()
+# b = input()
+# count = 0
+# for i in range(len(a)):
+#     if a[i:i+len(b)] == b:
+#         count += 1
+# print(count)
+# print(a.count(b))
 
 
 # print(compile.__doc__)

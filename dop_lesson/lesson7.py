8ee047546effd201c3110a566a7c6b1eec6764ed
evgenii <evgenii.lyabov@mail.ru>
2022-12-16 19:16:34 +0300

    adddoples8

--- dop_lesson/lesson7.py
+++ dop_lesson/lesson7.py
@@ -58,9 +58,94 @@
 # h = open('text2.txt')
 # print(h.read())                     # stroka С‚РµРєСЃС‚Р° кирилица отображается не правильно
 
-h = open('text2.txt', encoding='utf-8')
-print(h.read())             # stroka текста прописываем кодировку и все норм
+# h = open('text2.txt', encoding='utf-8')
+# print(h.read())             # stroka текста прописываем кодировку и все норм
 
-4,05,30
+"""Строки, экранированные символы"""
+"""множество"""
+# t = {'a', 'f', 1, 1, 1, 1, 2, 3, 25, (2, 5)}
+# y = set()
+# print(t)            # {1, 2, 3, 'a', 25, (2, 5), 'f'} содержит уникальные эл. и выводит врасброс
+""""""
+# x = (1, 2, 3, 4, 5, 6, 7)       # кортеж
+# y = [1, 2, 3, 4, 5, 6, 7]       # список
+# u = {'1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7}       # словарь
+# h = {1, 2, 3, 4, 5, 6, 7}       # множество
+
+# print(x.__sizeof__())        # (1, 2, 3, 4, 5, 6, 7)  80
+# print(y.__sizeof__())        # [1, 2, 3, 4, 5, 6, 7]  104
+# print(u.__sizeof__())        # {'1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7}   256
+# print(h.__sizeof__())        # {1, 2, 3, 4, 5, 6, 7}  456
+"""сравнение скорости работы списка и множества"""
+# import time
+# def f(*args):
+#     list_new = []
+#     for i in args:
+#         for y in i:
+#             if y not in list_new:
+#                 list_new.append(y)
+#     return list_new
+    
+# z = list(range(10000))
+# x = list(range(5000, 15000))
+# c = list(range(10000, 20000))
+# start = time.time()
+# f(z, x, c)
+# stop = time.time() - start
+# print(stop)                         # 2.4175496101379395 sec
+
+# start2 = time.time()
+# r = set(z)
+# t = r.union(set(x), set(c))
+# stop2 = time.time() - start2
+# print(stop2)                        # 0.002000570297241211 sec
+""""""
+# z = {1, 2, 3, 4, 5}
+# x = {3, 4, 5, 6, 7}
+# # z.add(6)    # добавляет эл. в множество {1, 2, 3, 4, 5, 6}
+# # z.discard(4)    # {1, 2, 3, 5, 6} удаляет эл
+# # z.remove(4)     # тоже что и discard
+# # y = z.union(x)    # объединяет множества{1, 2, 3, 4, 5, 6, 7}
+# # z.update(x)         # так же объединяет множ.{1, 2, 3, 4, 5, 6, 7}
+# # y = z.intersection(x)   # {3, 4, 5} находит общие эл. множ.
+# y = z.difference(x)         # {1, 2} определяет разность множ. эл. 1 2 в множ z  нет в множ. х
+
+# print(z)
+# print(y)
+"""применение множеств"""
+# r = open('dop_lesson/textles7.txt', encoding='utf-8')
+# # print(r.read().split()) # выводит список из всех слов без пробелов и знаков переноса строки: ['-', 'Очень', 'просто.', 'Хотя',
+# print(set(r.read().split()))    # конвертруем список в множ. откидывает все повторяющиеся {'пришел,', 'Зашел', 'А', 'покрасился.',
+""""""
+# new = set()     # создаем новый словарь
+# r = open('dop_lesson/textles7.txt', encoding='utf-8')
+# new.update(set(r.read().split()))       # щбъединяем
+# r.close()
+# r = open('dop_lesson/textles71.txt', encoding='utf-8')
+# new.update(set(r.read().split()))       # щбъединяем
+# r.close()
+# print(new)  # {'Хотя!..', 'варана.', 'воды', 'там', получаем уник. слова с обоих текстов
+"""нахождение одинаковых слов в обоих текстах"""
+# r = open('dop_lesson/textles7.txt', encoding='utf-8')
+# r1 = set(r.read().split())
+# r.close()
+# r = open('dop_lesson/textles71.txt', encoding='utf-8')
+# r2 = set(r.read().split())
+# r.close()
+# new = r1.intersection(r2)
+# print(new)  # {'вот', 'его', 'в', 'а', 'с', 'время', 'на', 'и', 'это', 'тот', 'он',
+            #'Но', 'не', 'немного', 'как', 'я', 'у', 'был', 'этот', 'что', 'по'}
+""""""
+# r = open('dop_lesson/textles7.txt', encoding='utf-8')
+# r1 = set(r.read().split())
+# r.close()
+# r = open('dop_lesson/textles71.txt', encoding='utf-8')
+# r2 = set(r.read().split())
+# r.close()
+# new = r1.difference(r2)
+# print(new)  # {'ужина.', 'полтора', 'паре', \\ слова из r1 которых нет в r2
+
+
+
 
 
